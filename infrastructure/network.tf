06c9ec21a7d5aa0f04f656f4d35979ebdafd2bb5
Oupman Miralles <oupman.miralles@qmr.cat>
2026-05-18 19:14:16 +0200

    bomb DynamoDB

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -23,6 +23,13 @@ resource "aws_security_group" "sd_task2_sg" {
     cidr_blocks = ["0.0.0.0/0"] # Web Dashboard RabbitMQ
   }
 
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # PostgresSQL
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
