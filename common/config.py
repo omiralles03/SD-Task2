395a73d08d6cf7cf3425ce79b6658f14c167cd47
Oupman Miralles <oupman.miralles@qmr.cat>
2026-05-26 19:32:58 +0200

    postgres vars

--- common/config.py
+++ common/config.py
@@ -8,3 +8,9 @@ RABBIT_HOST = os.getenv("RABBIT_HOST", "localhost")
 
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 RABBIT_PORT = int(os.getenv("RABBIT_PORT", 5672))
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "ticket_system")
+DB_USER = os.getenv("DB_USER", "user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "123")
+DB_PORT = int(os.getenv("DB_PORT", 5432))
