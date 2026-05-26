7369c7e124d9d054ee7feeb6fb1c6944273fc817
Oupman Miralles <oupman.miralles@qmr.cat>
2026-05-26 19:35:21 +0200

    idk, but doesnt work as intended

--- controller/controller.py
+++ controller/controller.py
@@ -1,70 +1,91 @@
+import base64
 import pika
 import boto3
 import time
-
-from pika.compat import time_now
+import json
 from common.config import RABBIT_HOST
 
 TRT = 10.0  # Tr (Target Response Time): objective time to empty the queue
 WC = 10.0 # C: worker capacity per second (1 msg/100ms --> C = 10mgs/s)
+BATCH_SIZE = 500 # num msg to send to each Lambda
 
 def monitor_and_scale():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBIT_HOST))
     channel = connection.channel()
 
     lambda_client = boto3.client('lambda', region_name='us-east-1')
+
     last_backlog = 0
-    
+
     st = time.perf_counter()
+
     try:
         while True:
 
             # Check the queue in passive mode and get the backlog
             queue = channel.queue_declare(queue='ticket_queue', durable=True, passive=True)
             backlog = queue.method.message_count
-            
+
             if backlog > 0:
                 # Arrival rate (λ) since last second
                 arrival_rate = max(backlog - last_backlog, 0)
-                
+
                 # N = [B + (λ * Tr)] / C
                 num_workers = (backlog + (arrival_rate * TRT)) / WC
-                
-                #  Limit to 200 max concurrency
-                num_workers_needed = max(1, min(int(num_workers) + 1, 200))
-                
+
+                #  Limit to 10 max concurrency
+                num_workers_needed = max(1, min(int(num_workers) + 1, 8))
+
                 et = time.perf_counter() - st
+
                 print(f"({et:.2f}s) [Load] Backlog (B): {backlog} | Arrival Rate (λ): {arrival_rate}/s")
                 print(f"({et:.2f}s) [Scale] Workers: {num_workers_needed}.")
-                
-                try:
-                    lambda_client.put_function_concurrency(
-                            FunctionName='TicketWorkerLambda',
-                            ReservedConcurrentExecutions=num_workers_needed
+
+                for _ in range(num_workers_needed):
+                    ticket_batch = []
+
+                    for _ in range(BATCH_SIZE):
+                        method, properties, body = channel.basic_get(queue='ticket_queue', auto_ack=True)
+
+                        if method:
+                            body_b64 = base64.b64encode(body).decode('utf-8')
+                            ticket_batch.append({"data": body_b64})
+                        else:
+                            break
+
+
+                    if ticket_batch:
+                        payload = {
+                            "rmqMessagesByQueue": { 
+                                "ticket_queue": ticket_batch
+                            }
+                        }
+
+                        try:
+                            lambda_client.invoke(
+                                FunctionName='TicketWorkerLambda',
+                                InvocationType='Event',
+                                Payload=json.dumps(payload)
                             )
-                except Exception as aws_err:
-                    print(f" [!] AWS Error: {aws_err}")
+
+                            # lambda_client.put_function_concurrency(
+                            #         FunctionName='TicketWorkerLambda',
+                            #         ReservedConcurrentExecutions=num_workers_needed
+                            #         )
+
+                        except Exception as aws_err:
+                            print(f" [!] AWS Error: {aws_err}")
 
                 last_backlog = backlog
             else:
-                if last_backlog > 0:
-                    print(f" [>] Queue empty.")
-
-                    try:
-                        lambda_client.delete_function_concurrency(
-                                FunctionName='TicketWorkerLambda'
-                                )
-                    except:
-                        pass
                 last_backlog = 0
-                
+
             time.sleep(1)
-            
+
     except KeyboardInterrupt:
         print(" [-] Controlador aturat.")
     finally:
         connection.close()
 
 if __name__ == "__main__":
-    monitor_and_scale()
-
+    monitor_and_scale() 
--- worker/lambda_function.py
+++ worker/lambda_function.py
@@ -3,108 +3,68 @@ import os
 import json
 import base64
 import psycopg2
-from psycopg2 import IntegrityError
 
-# Database connection (Outside the handler to reuse it between executions and be more efficient)
 DB_HOST = os.environ.get("DB_HOST")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_PORT = os.environ.get("DB_PORT")
 
 def get_db_connection():
     return psycopg2.connect(
         host=DB_HOST,
         database=DB_NAME,
         user=DB_USER,
-        password=DB_PASSWORD
+        password=DB_PASSWORD,
+        port=DB_PORT
     )
 
 def lambda_handler(event, context):
-    # Start time for metrics
-    start_time = time.time()
-    
-    # Artificial Delay: external payment latency
-    time.sleep(0.1)
-    
-    conn = None
-    try:
-        conn = get_db_connection()
-        cur = conn.cursor()
-        
+    # AWS sends data as in format:
+    #   event = { "rmqMessagesByQueue": { "ticket_queue": [ {"data": "xxx"}, {"data": "yyy"} ] }}
+    msg_list = event.get('rmqMessagesByQueue', {}).get('ticket_queue', [])
+
+    # Connection with PostgreSQL
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    for msg in msg_list:
+        start_time = time.time()
+
         # When AWS reads from RabbitMQ, it sends the messages inside 'rmqMessagesByQueue'
         # The messages come encoded in Base64.
-        if "rmqMessagesByQueue" in event:
-            for queue_name, messages in event["rmqMessagesByQueue"].items():
-                for msg in messages:
-                    # Decode the message sent by producer.py
-                    decoded_data = base64.b64decode(msg['data']).decode('utf-8')
-                    ticket_data = json.loads(decoded_data)
-                    
-                    process_ticket(cur, conn, ticket_data)
-        else:
-            # Fallback for manual testing sending a direct JSON from the AWS console
-            process_ticket(cur, conn, event)
-            
-        conn.commit()
+        body_data = base64.b64decode(msg['data']).decode('utf-8')
+        payload = json.loads(body_data)
 
-    except Exception as e:
-        if conn:
-            conn.rollback()
-        print(f"Transaction error: {e}")
-        raise e
-        
-    finally:
-        # Metrics: record transaction time
-        end_time = time.time()
-        duration = end_time - start_time
-        
-        if conn:
-            try:
-                cur = conn.cursor()
-                # Save to the database to compute Throughput and Latency later
-                cur.execute("""
-                    INSERT INTO transaction_metrics (start_time, end_time, duration) 
-                    VALUES (to_timestamp(%s), to_timestamp(%s), %s);
-                """, (start_time, end_time, duration))
-                conn.commit()
-            except Exception as metric_err:
-                print(f"Error saving metrics: {metric_err}")
-            finally:
-                cur.close()
-                conn.close()
+        time.sleep(0.1)
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Processing completed successfully')
-    }
-
-# Purchase and concurrency logic
-def process_ticket(cur, conn, ticket_data):
-    ticket_type = ticket_data.get("type")
-    request_id = ticket_data.get("request_id") # Key for idempotency
-    
-    if ticket_type == "unnumbered":
-        # We do an UPDATE. The condition "available > 0" ensures no overselling
-        cur.execute("""
-            UPDATE unnumbered_tickets 
-            SET available = available - 1 
-            WHERE id = 1 AND available > 0 
-            RETURNING available;
-        """)
-        result = cur.fetchone()
-        if not result:
-            print(f"[{request_id}] Denied: No unnumbered tickets left.")
-            
-    elif ticket_type == "numbered":
-        seat_id = ticket_data.get("seat_id")
-        try:
-            # We do an INSERT. If the seat already exists, it will raise an IntegrityError 
-            # assuming the seat_id column has a UNIQUE constraint in PostgreSQL.
+        if payload['action'] == 'buy_unnumbered':
             cur.execute("""
-                INSERT INTO occupied_seats (seat_id, request_id) 
+                UPDATE unnumbered_tickets
+                SET sold_tickets = sold_tickets + 1
+                WHERE sold_tickets < total_tickets;
+            """)
+
+        elif payload['action'] == 'buy_numbered':
+            cur.execute("""
+                INSERT INTO numbered_tickets (seat_number, client_id)
                 VALUES (%s, %s);
-            """, (seat_id, request_id))
-        except IntegrityError:
-            # We handle the error to avoid double-selling and rollback the failed insert
-            conn.rollback()
-            print(f"[{request_id}] Denied: Seat {seat_id} is already sold.")
+            """, (
+                payload['seat_number'],
+                payload['client_id']
+            ))
+
+        end_time = time.time()
+
+        cur.execute("""
+            INSERT INTO metrics (request_id, action_type, start_time, end_time, latency)
+            VALUES (%s, %s, %s, %s, %s);
+        """, (
+            payload['request_id'],
+            payload['action'],
+            start_time,
+            end_time,
+            end_time - start_time
+        ))
+
+    conn.commit() 
