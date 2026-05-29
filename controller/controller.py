08121c929f306fc2ed9ea7ac458c1a74a8b7a604
Oupman Miralles <oupman.miralles@qmr.cat>
2026-05-29 18:58:46 +0200

    fuck u dani

--- controller/controller.py
+++ controller/controller.py
@@ -1,8 +1,7 @@
-import base64
 import pika
 import boto3
 import time
-import json
+import subprocess
 from common.config import RABBIT_HOST
 
 # Constantly observes the amount of messages that exist in rabbitmq and decides how many lambdas should be executed.
@@ -10,6 +9,7 @@ from common.config import RABBIT_HOST
 TRT = 10.0  # Tr (Target Response Time): objective time to empty the queue
 WC = 10.0 # C: worker capacity per second (1 msg/100ms --> C = 10mgs/s)
 BATCH_SIZE = 500 # num msg to send to each Lambda
+MAX_WORKERS = 40
 
 def monitor_and_scale():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBIT_HOST))
@@ -21,8 +21,13 @@ def monitor_and_scale():
 
     st = time.perf_counter()
 
+    active_workers = []
+    curr_workers = 0
+
     try:
         while True:
+            active_workers = [p for p in active_workers if p.poll() is None]
+            curr_workers_count = len(active_workers)
 
             # Check the queue in passive mode and get the backlog
             queue = channel.queue_declare(queue='ticket_queue', durable=True, passive=True)
@@ -36,50 +41,27 @@ def monitor_and_scale():
                 # N = [B + (λ * Tr)] / C
                 num_workers = (backlog + (arrival_rate * TRT)) / WC
 
-                #  Limit to 8 max concurrency
-                num_workers_needed = max(1, min(int(num_workers) + 1, 8))
+                #  Limit to 40 max concurrency
+                num_workers_needed = max(1, min(int(num_workers) + 1, MAX_WORKERS))
 
                 et = time.perf_counter() - st
 
                 # Log
                 print(f"({et:.2f}s) [Load] Backlog (B): {backlog} | Arrival Rate (λ): {arrival_rate}/s")
-                print(f"({et:.2f}s) [Scale] Workers: {num_workers_needed}.")
+                print(f"({et:.2f}s) [Scale] Workers needed: {num_workers_needed}.")
+                print(f"({et:.2f}s) [Active] Workers active: {curr_workers_count}.")
 
-                # Extract an amount of messages per worker equal to BATCH_SIZE
-                for _ in range(num_workers_needed):
-                    ticket_batch = []
+                if num_workers_needed > curr_workers_count:
+                    workers_to_launch = num_workers_needed - curr_workers_count
+                    print(f"({et:.2f}s) [Scale] Workers to launch: {workers_to_launch}.")
 
-                    for _ in range(BATCH_SIZE):
-                        method, properties, body = channel.basic_get(queue='ticket_queue', auto_ack=True)
-
-                        if method:
-                            body_b64 = base64.b64encode(body).decode('utf-8')
-                            ticket_batch.append({"data": body_b64})
-                        else:
-                            break
-
-
-                    if ticket_batch:
-                        payload = {
-                            "rmqMessagesByQueue": { 
-                                "ticket_queue": ticket_batch
-                            }
-                        }
-
-                        try:
-                            lambda_client.invoke(
-                                FunctionName='TicketWorkerLambda',
-                                InvocationType='Event', # Asíncrona
-                                Payload=json.dumps(payload)
-                            )
-
-                            # lambda_client.put_function_concurrency(
-                            #         FunctionName='TicketWorkerLambda',
-                            #         ReservedConcurrentExecutions=num_workers_needed
-                            #         )
-
-                        except Exception as aws_err:
-                            print(f" [!] AWS Error: {aws_err}")
+                    for _ in range(workers_to_launch):
+                        curr_workers += 1
+                        worker_id = f"{curr_workers}"
+                        p = subprocess.Popen(["python3", "-u", "-m", "worker.worker", worker_id])
+                        active_workers.append(p)
+                else:
+                    print(f"({et:.2f}s) [Scale] Enough active_workers (active: {curr_workers_count}, needed: {num_workers_needed}")
 
                 last_backlog = backlog
             else:
@@ -88,7 +70,9 @@ def monitor_and_scale():
             time.sleep(1)
 
     except KeyboardInterrupt:
-        print(" [-] Controlador aturat.")
+        print(" [-] Controller Stopped.")
+        for p in active_workers:
+            p.terminate()
     finally:
         connection.close()
 
